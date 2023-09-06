e275adb9e7bdf0a97f3538d86edc2da33b163f25
Breno <onerbreno@outlook.com>
2023-09-05 23:21:56 -0300

    docs Add text input component

--- packages/react/src/components/Box.tsx
+++ packages/react/src/components/Box.tsx
@@ -5,7 +5,7 @@ export const Box = styled('div', {
   padding: '$4',
   borderRadius: '$md',
   background: '$gray800',
-  border: '1px solid $gray900',
+  border: '1px solid $gray600',
 })
 
 export interface BoxProps extends ComponentProps<typeof Box> {
